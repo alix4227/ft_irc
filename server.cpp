310e5605a29b3f9376292267a7650ce4723a78bd
alix4227 <alix_07@hotmail.fr>
2025-11-09 19:41:52 +0100

    creation feature accept connexion

--- server.cpp
+++ server.cpp
@@ -9,6 +9,28 @@
 #include <cstring>
 #include<vector>
 #include <sys/poll.h>
+
+void accept_new_connection(int server_socket, std::vector<pollfd>& poll_fds, struct sockaddr_in socketAddress, socklen_t socketAdressLength)
+{
+	(void)poll_fds;
+	char msg_to_send[BUFFER_SIZE];
+    int connectedSocketFD = accept(server_socket,(struct sockaddr*)&socketAddress, &socketAdressLength);
+	if (connectedSocketFD == -1)
+	{
+		std::cerr << "(Serveur)echec d'etablissement de la connexion" << std::endl;
+		return ;
+	}
+    // add_to_poll_fds(poll_fds, client_fd, poll_count, poll_size);
+	std::cout << "[Server] Accepted new connection on client socket " <<  connectedSocketFD << std::endl;
+	sprintf(msg_to_send, "Welcome. You are client fd [%d]\n", connectedSocketFD);
+    int status = send(connectedSocketFD, msg_to_send, strlen(msg_to_send), 0);
+    if (status == -1) 
+	{
+		std::cout << "[Server] Send error to client " << connectedSocketFD << ": " << "message not send" << std::endl;
+		return ;
+    }
+}
+
 int main (void)
 {
 	//SOCK STREAM c'est pour indiquer qu'on va utiliser le protocole TCP
@@ -36,13 +58,52 @@ int main (void)
 		std::cerr << "(Serveur)echec de demarage de l'ecoute des connexions entrantes" << std::endl;
 		exit (1);
 	}
-	std::cout << "En attente de nouvelles connexions..." << std::endl;
-
+	
 	std::vector<pollfd>poll_fds;
 	pollfd p;
 	p.fd = socketFD;
 	p.events = POLLIN;
 	poll_fds.push_back(p);
+	
+    while (1) 
+	{ 
+        // Sonde les sockets prêtes (avec timeout de 2 secondes)
+        int status = poll(poll_fds.data(), poll_fds.size(), 2000);
+        if (status == -1) 
+		{
+           std::cerr << "(Serveur)echec de poll" << std::endl;
+            exit(1);
+        }
+        else if (status == 0) 
+		{
+            // Aucun descipteur de fichier de socket n'est prêt
+            std::cout << "En attente de nouvelles connexions..." << std::endl;
+            continue;
+        }
+
+        // Boucle sur notre tableau de sockets
+		
+        for (size_t i = 0; i < poll_fds.size(); ++i) 
+		{
+            if (!(poll_fds[i].revents & POLLIN)) 
+			{
+				// La socket n'est pas prête à être lue
+				// on s'arrête là et on continue la boucle
+				continue ;
+            }
+			std::cout << "[" << socketFD << "]" << "Ready for I/O operation\n" << std::endl;
+           
+            // La socket est prête à être lue !
+            if (poll_fds[i].fd == socketFD) {
+                // La socket est notre socket serveur qui écoute le port
+                accept_new_connection(socketFD, poll_fds, socketAddress, socketAdressLength);
+            }
+            // else {
+            //     // La socket est une socket client, on va la lire
+            //     read_data_from_socket(i, &poll_fds, poll_fds.size(), socketFD);
+            // }
+        }
+    }	
 
 
 
@@ -51,31 +112,30 @@ int main (void)
 
 
 
+	// int connectedSocketFD = accept(socketFD,(struct sockaddr*)&socketAddress, &socketAdressLength);
+	// if (connectedSocketFD == -1)
+	// {
+	// 	std::cerr << "(Serveur)echec d'etablissement de la connexion" << std::endl;
+	// 	exit (1);
+	// }
+	// //On va utiliser la memoire tampon pour receptionner le message
+	// char buffer[BUFFER_SIZE] = {0};
+	// int reveivedBytes = recv(connectedSocketFD, buffer, BUFFER_SIZE, 0);
+	// if (reveivedBytes == -1)
+	// {
+	// 	std::cerr << "(Serveur)echec de reception du message du client" << std::endl;
+	// 	exit (1);
+	// }
+	// std::cout << "Client: "<< buffer << std::endl;
 
-	int connectedSocketFD = accept(socketFD,(struct sockaddr*)&socketAddress, &socketAdressLength);
-	if (connectedSocketFD == -1)
-	{
-		std::cerr << "(Serveur)echec d'etablissement de la connexion" << std::endl;
-		exit (1);
-	}
-	//On va utiliser la memoire tampon pour receptionner le message
-	char buffer[BUFFER_SIZE] = {0};
-	int reveivedBytes = recv(connectedSocketFD, buffer, BUFFER_SIZE, 0);
-	if (reveivedBytes == -1)
-	{
-		std::cerr << "(Serveur)echec de reception du message du client" << std::endl;
-		exit (1);
-	}
-	std::cout << "Client: "<< buffer << std::endl;
-
-	const char message[] = "Bonjour, je suis le serveur!";
-	int sentBytes = send(connectedSocketFD, message, strlen(message), 0);
-	if (sentBytes == -1)
-	{
-		std::cerr << "(Serveur)echec de l'envoi du message au client" << std::endl;
-		exit (1);
-	}
-	close(connectedSocketFD);
+	// const char message[] = "Bonjour, je suis le serveur!";
+	// int sentBytes = send(connectedSocketFD, message, strlen(message), 0);
+	// if (sentBytes == -1)
+	// {
+	// 	std::cerr << "(Serveur)echec de l'envoi du message au client" << std::endl;
+	// 	exit (1);
+	// }
+	// close(connectedSocketFD);
 	close(socketFD);
 	return (0);
 
